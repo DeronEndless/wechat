59aa861ff8eb8b45e8cc5357200189c08f222da9
zhaofg <zhaofg@glodon.com>
2019-03-21 12:53:08 +0800

    bug fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var Koa = require('Koa')
+var Koa = require('koa')
 var sha1 = require('sha1')
 
 var config = {
@@ -16,21 +16,23 @@ app.use(async (ctx, next) => {
         let token = config.wechat.token
         let signature = ctx.query.signature
         let nonce = ctx.query.nonce
-        let timeStamp = ctx.query.timeStamp
-        let ecostr = ctx.query.ecostr
-        let tmpStr = [token, timeStamp, nonce].sort().join('')
-        let shaStr = sha1(tmpStr)
-        if (shaStr === signature) {
-            ctx.body = ecostr + ''
+        let timestamp = ctx.query.timestamp
+        let echostr = ctx.query.echostr
+        let str = [token, timestamp, nonce].sort().join('')
+        console.log('str', str)
+        let sha = sha1(str)
+        console.log('sha', sha)
+        console.log('signature', signature)
+        if (sha === signature) {
+            ctx.body = echostr + ''
         } else {
             ctx.body = 'error'
         }
-
     } catch (err) {
         ctx.body = { message: err.message }
         ctx.status = err.status || 500
     }
 })
 
-app.listen(7777)
-console.log('Listening 7777')
+app.listen(7788)
+console.log('Listening 7788')
