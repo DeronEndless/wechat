6905ed6a755307ce7e4584d02e26b53b25e4d724
zhaofg <zhaofg@glodon.com>
2019-03-23 23:44:04 +0800

    test

--- wechat/g.js
+++ wechat/g.js
@@ -1,74 +1,46 @@
 const sha1 = require('sha1')
-const Promise = require('bluebird')
-const request = Promise.promisify(require('request'))
-const prefix = 'https://api.weixin.qq.com/cgi-bin/token'
-const api = {
-  accessToken: `${prefix}?grant_type=client_credential`
-}
-function Wechat(opts) {
-  const that = this
-  this.appId = opts.appId
-  this.appSecret = opts.appSecret
-  this.getAccessToken = opts.getAccessToken
-  this.saveAccessToken = opts.saveAccessToken
-  
-  this.getAccessToken().then(data => {
-    try {
-      data = JSON.parse(data)
-    } catch (error) {
-      return that.updataAccessToken()
-    }
-    if (that.isValidAccessToken(data)) {
-      Promise.resolve(data)
-    } else {
-      return that.updataAccessToken()
-    }
-  }).then(data => {
-    that.access_token = data.access_token
-    that.expires_in = data.expires_in
-    
-    that.saveAccessToken(data)
-  })
-}
-Wechat.prototype.updataAccessToken = function (data) {
-  let appId = this.appId
-  let appSecret = this.appSecret
-  let url = `${api.accessToken}&appid=${appId}&secret=${appSecret}`
-
-  return new Promise((resolve, reject) => {
-    request({url: url, json: true}).then(res => {
-      console.log('res.body', res.body)
-      console.log('res.body.expires_in', res.body.expires_in)
-      let data = res.body
-      let now = (new Date().getTime())
-      let expires_in = now + (data.expires_in - 20) * 1000
-      
-      data.expires_in = expires_in
-      resolve(data)
-    })
-  })
-}
+const getRawBody = require('raw-body')
+const Wechat = require('./wechat')
 
 module.exports = function (opts) {
   let wechat = new Wechat(opts)
-  return async (ctx) => {
+  return async ctx => {
     try {
-        console.log(ctx.query)
-        let token = opts.token
-        let signature = ctx.query.signature
-        let nonce = ctx.query.nonce
-        let timestamp = ctx.query.timestamp
-        let echostr = ctx.query.echostr
-        let str = [token, timestamp, nonce].sort().join('')
-        let sha = sha1(str)
+      console.log(ctx.query)
+      let token = opts.token
+      let signature = ctx.query.signature
+      let nonce = ctx.query.nonce
+      let timestamp = ctx.query.timestamp
+      let echostr = ctx.query.echostr
+
+      let str = [token, timestamp, nonce].sort().join('')
+      let sha = sha1(str)
+
+      if (ctx.methods === 'GET') {
+        console.log('ctx.methods', ctx.methods)
         if (sha === signature) {
-            ctx.body = echostr + ''
+          ctx.body = echostr + ''
         } else {
-            ctx.body = 'error'
+          ctx.body = 'error'
         }
+      } else if (ctx.methods === 'POST'){
+        console.log(1111)
+        if (sha !== signature) {
+          ctx.body = 'error'
+          return false
+        }
+        console.log('ctx.req', ctx.req)
+        let data = await getRawBody(ctx.req, {
+          length: ctx.length,
+          limit: '1mb',
+          encoding: ctx.charset
+        })
+        console.log('data', data.toString())
+      }
+
     } catch (err) {
-        ctx.body = { message: err.message }
-        ctx.status = err.status || 500
+      ctx.body = { message: err.message }
+      ctx.status = err.status || 500
     }
   }
 }
---
+++ wechat/wechat.js
@@ -0,0 +1,62 @@
+const Promise = require('bluebird')
+const request = Promise.promisify(require('request'))
+const prefix = 'https://api.weixin.qq.com/cgi-bin/token'
+const api = {
+  accessToken: `${prefix}?grant_type=client_credential`
+}
+function Wechat(opts) {
+  const that = this
+  this.appId = opts.appId
+  this.appSecret = opts.appSecret
+  this.getAccessToken = opts.getAccessToken
+  this.saveAccessToken = opts.saveAccessToken
+  
+  this.getAccessToken().then(data => {
+    try {
+      data = JSON.parse(data)
+    } catch (error) {
+      return that.updataAccessToken()
+    }
+    if (that.isValidAccessToken(data)) {
+      that.access_token = data.access_token
+      that.expires_in = data.expires_in
+      console.log('accessToken Data', data)
+      that.saveAccessToken(data)
+    } else {
+      return that.updataAccessToken()
+    }
+  })
+}
+Wechat.prototype.isValidAccessToken = function (data) {
+  if (!data || !data.access_token || !data.expires_in) {
+    return false
+  }
+  let access_token = data.access_token
+  let expires_in = data.expires_in
+  let now = (new Date().getTime())
+
+  if (now < expires_in) {
+    return true
+  } else {
+    return false
+  }
+}
+Wechat.prototype.updataAccessToken = function (data) {
+  let appId = this.appId
+  let appSecret = this.appSecret
+  let url = `${api.accessToken}&appid=${appId}&secret=${appSecret}`
+
+  return new Promise((resolve, reject) => {
+    request({url: url, json: true}).then(res => {
+      console.log('res.body', res.body)
+      console.log('res.body.expires_in', res.body.expires_in)
+      let data = res.body
+      let now = (new Date().getTime())
+      let expires_in = now + (data.expires_in - 20) * 1000
+      
+      data.expires_in = expires_in
+      resolve(data)
+    })
+  })
+}
+module.exports = Wechat
